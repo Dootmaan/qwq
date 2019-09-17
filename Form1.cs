838c5ec180a9b91f6b16d154082a3201f84a2dc8
SamWoog <njdswhy@gmail.com>
2019-09-17 11:53:37 +0800

    version 1.0.0

---
+++ Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qwq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            //
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CreditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form2().ShowDialog();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            Clipboard.SetText(button1.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button3.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button4.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button5.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button2.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button6.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button7.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button8.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button9.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button10_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button10.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button11_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button11.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button12_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button12.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button13_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button13.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button14.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button15_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button15.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button16.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button17_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button17.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button18_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button18.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button19_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button19.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button20_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button20.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button25_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button25.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button24_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button24.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button23_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button23.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button22_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button22.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button21_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button21.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button26_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button26.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button27_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button27.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button28_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button28.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button29_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button29.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button30_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button30.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button35_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button35.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button40_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button40.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button39_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button39.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button38_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button38.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button37_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button37.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button36_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button36.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button31_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button31.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button32_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button32.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button33_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button33.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button34_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button34.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button45_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button45.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button44_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button44.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button43_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button43.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button42_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button42.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button41_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button41.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button46_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button46.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button47_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button47.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button48_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button48.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button49_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button49.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void Button50_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(button50.Text);
+            
+            SendKeys.Send("%{TAB}");System.Threading.Thread.Sleep(500);SendKeys.Send("^v");
+        }
+
+        private void AddToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            new Form3().ShowDialog();
+        }
+
+        private void ReportIssuesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto: njdswhy@163.com");
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace qwq
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ˊ_>ˋ旦\r\ndotman(c)2019";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 169);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form2";
+            this.Text = "Credits";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
